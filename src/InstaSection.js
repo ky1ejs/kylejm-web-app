37b2247ec52ec3be21d6367ddfa1d014ad872e95
Phillip Boateng <cnolimit@gmail.com>
2018-11-30 12:38:51 +0000

    fixed styling issues with the insta images

--- src/InstaSection.js
+++ src/InstaSection.js
@@ -1,38 +1,50 @@
-import React, { Component } from 'react'
-import styled from 'styled-components'
+import React, { Component } from "react";
+import styled from "styled-components";
 
 const Wrapper = styled.div`
   width: 100%;
-  max-width: 1200px;
-  margin: 0 auto;
+  max-width: calc(80% + 100px);
   display: flex;
   overflow-x: scroll;
+  align-self: flex-end;
+  margin-top: 25px;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
-const Image = styled.img`
-  margin: 0 20px;
-  object-fit: contain;
+const Image = styled.div`
   height: 250px;
-  width: 250px;
+  min-width: 250px;
+  display: inline-block;
+  background-image: url(${props => props.src});
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
+
+  &:not(:first-child) {
+    margin: 0 20px;
+  }
 `;
 
 export default class InstaSection extends Component {
-  state = { urls: [] }
+  state = { urls: [] };
 
   componentDidMount() {
-      fetch('https://kylejm.herokuapp.com/v1/insta')
+    fetch("https://kylejm.herokuapp.com/v1/insta")
       .then(response => response.json())
-      .then((json) => json.data.map((post) => post.images.low_resolution.url))
-      .then((urls) => this.setState({ urls: urls }))
-    }
-
-    render() {
-      return (
-        <Wrapper>
-          {
-            this.state.urls.map((url) => <Image key={url} src={url} />)
-          }
-        </Wrapper>
-      );
-    }
+      .then(json => json.data.map(post => post.images.low_resolution.url))
+      .then(urls => this.setState({ urls: urls }));
   }
+
+  render() {
+    return (
+      <Wrapper>
+        {this.state.urls.map(url => (
+          <Image key={url} src={url} />
+        ))}
+      </Wrapper>
+    );
+  }
+}

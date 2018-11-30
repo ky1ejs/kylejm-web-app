bf26013838e8ea6acf8d9ac4bc65a79312412488
Phillip Boateng <cnolimit@gmail.com>
2018-11-30 12:38:19 +0000

    applied flex properties to the main container

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React, { Component } from 'react'
-import IntroSection from './IntroSection.js'
-import InstaSection from './InstaSection.js'
+import React, { Component } from "react";
+import IntroSection from "./IntroSection.js";
+import InstaSection from "./InstaSection.js";
+import styled from "styled-components";
+
+const Container = styled.div`
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+`;
 
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Container>
         <IntroSection />
         <InstaSection />
-      </div>
-    )
+      </Container>
+    );
   }
 }
